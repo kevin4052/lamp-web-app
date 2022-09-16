c9dce123e8b8ec1572daccc2fc8ee4d09e5ed3b5
Kevin Hernandez <kevin.hernandez001@gmail.com>
2022-09-16 09:46:28 -0400

    signup page and auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,41 @@
-<x-guest-layout>
+@extends('layouts.landing') 
+
+@section('page-title', 'login') 
+
+@section('content')
+<div class="form-signin w-100 m-auto text-center">
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
+        <h1 class="h3 mb-3 fw-normal">Login</h1>    
+        <div class="form-floating">
+          <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com" value="{{old('email')}}">
+          <label for="email">Email address</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" name="password" class="form-control" id="password" placeholder="Password" required autocomplete="current-password">
+          <label for="password">Password</label>
+        </div>
+        <!-- Remember Me -->
+        <div class="block mt-1 mb-1">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+        <div class="block mt-1 mb-1">
+            @if (Route::has('password.request'))
+                <a href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+        </div>    
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+    </form>
+    <p>Don't have an account? <a href="/register">Signup</a></p>
+</div>
+@endsection
+
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -53,4 +90,4 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout> --}}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,65 @@
-<x-guest-layout>
+@extends('layouts.landing')
+@section('page-title', 'signup')
+
+@section('content')
+<div class="form-signin w-100 m-auto text-center">
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+        <h1 class="h3 mb-3 fw-normal">Signup</h1>
+
+        <div class="form-floating">
+            <input id="firstname" type="text" class="form-control" name="firstname" placeholder="John" value="{{old('firstname')}}" required autofocus>
+            <label for="firstname">First Name</label>
+            @error('firstname')
+            <div class="alert alert-danger" role="alert">
+                    {{$message}}
+                </div>
+            @enderror
+        </div>
+
+        <div class="form-floating">
+            <input id="lastname" type="text" class="form-control" name="lastname" placeholder="Doe" value="{{old('lastname')}}" required autofocus>
+            <label for="lastname">Last Names</label>
+            @error('lastname')
+            <div class="alert alert-danger" role="alert">
+                    {{$message}}
+                </div>
+            @enderror
+        </div>
+    
+        <div class="form-floating">
+          <input id="email" type="email" class="form-control" name="email" id="floatingInput" placeholder="name@example.com" value="{{old('email')}}" required autofocus>
+          <label for="email">Email address</label>
+          @error('email')
+                <div class="alert alert-danger" role="alert">
+                    {{$message}}
+                </div>
+            @enderror
+        </div>
+
+        <div class="form-floating">
+          <input id="password" type="password" class="form-control" name="password" id="floatingPassword" placeholder="Password" required autocomplete="new-password"> 
+          <label for="password">Password</label>
+          @error('password')
+                <div class="alert alert-danger" role="alert">
+                    {{$message}}
+                </div>
+            @enderror
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="form-floating">
+            <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" id="floatingPassword" placeholder="Password" required> 
+            <label for="password_confirmation">Confirm Password</label>
+        </div>
+   
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+    </form>
+    <p>Already have an account? <a href="/login">Login</a></p>
+</div>
+@endsection
+
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -63,4 +124,4 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout> --}}
