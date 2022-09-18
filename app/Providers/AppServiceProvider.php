d0b61766e44dc50931a9aa16556fdae91a1b01d7
Kevin Hernandez <kevin.hernandez001@gmail.com>
2022-09-18 15:40:55 -0400

    force https on all routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        URL::forceScheme('https');
+        Schema::defaultStringLength(191);
         Paginator::useBootstrapFive();
     }
 }
